62db34f9cf0c7645f9b92efb5a09a1c96f1c31a9
tjumyk <tjumyk@gmail.com>
2020-09-18 14:23:55 +0800

    add comments about another way to fix pdfjs worker

--- hotfix_pdfjs_worker.sh
+++ hotfix_pdfjs_worker.sh
@@ -1,1 +1,10 @@
 sed -i 's/return new Worker[(][a-zA-Z.]*/return new Worker("static_cn\/"/g' static_cn/main*.js
+
+# Another way to hotfix is as follows, which might not work with IE (need testing).
+# In pdfjs-dist/webpack.js, use the following code for loading worker.
+#
+# var PdfjsWorker = require('!!raw-loader!./build/pdf.worker.min.js');
+#
+# if (typeof window !== 'undefined' && 'Worker' in window) {
+#   pdfjs.GlobalWorkerOptions.workerSrc = window.URL.createObjectURL(new Blob([PdfjsWorker.default],{type:'text/javascript'}));
+# }

d167f1b7385f4098483cb62bc1deba82e8e42de6
tjumyk <tjumyk@gmail.com>
2020-12-10 20:52:07 +0800

    try binned total chart

--- angular/src/app/marking-summary-charts/marking-summary-charts.component.ts
+++ angular/src/app/marking-summary-charts/marking-summary-charts.component.ts
@@ -46,7 +46,7 @@ export class MarkingSummaryChartsComponent implements OnInit {
 
     for(let q of this.task.questions){
       marksList = questionMarksList[q.id];
-      if(!marksList)
+      if (!marksList)
         continue
       this.data.push({
         question: q,
@@ -55,10 +55,10 @@ export class MarkingSummaryChartsComponent implements OnInit {
       })
     }
 
-    if(this.data.length > 1){
+    if (this.data.length > 1) {
       this.data.push({
         question: null,
-        chartData: this.buildBarChart(totalMarksList),
+        chartData: this.buildBinnedBarChart(totalMarksList, 10),
         isTotal: true
       })
     }
@@ -92,6 +92,104 @@ export class MarkingSummaryChartsComponent implements OnInit {
     for (let v of numberList) {
       labels.push('' + v);
       data.push(numberCounts[v])
+    }
+
+    if (nanCount > 0) {
+      labels.unshift('NaN');
+      data.unshift(nanCount);
+    }
+
+    if (noResultCount > 0) {
+      labels.unshift('N/A');
+      data.unshift(noResultCount);
+    }
+
+    let datasetLabel = 'Students';
+
+    return {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: datasetLabel,
+            data: data
+          }
+        ]
+      },
+      options: {
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                precision: 0
+              }
+            }
+          ]
+        }
+      }
+    }
+  }
+
+  private buildBinnedBarChart(results: number[], maxNumOfBins: number) {
+    let numOfBins, binSize;
+    if (!results || !results.length) { // fallback default
+      binSize = 1;
+      numOfBins = 1;
+    } else {
+      let max = results[0];
+      for (let v of results) {
+        if (v > max)
+          max = v;
+      }
+      const candidateBinSizes = [1, 5, 10, 20, 50, 100];
+      for (let i = 0; i < candidateBinSizes.length; i++) {
+        binSize = candidateBinSizes[i];
+        if (max / binSize <= maxNumOfBins) {
+          break;
+        }
+      }
+      while (binSize * maxNumOfBins < max) {
+        binSize *= 2;
+      }
+      numOfBins = Math.ceil(max / binSize);
+      if (numOfBins * binSize == max) { // in case the max is exactly at the boundary, save one bin
+        --numOfBins;
+      }
+    }
+
+    let binCounts = new Array(numOfBins).fill(0);
+    let noResultCount = 0;
+    let nanCount = 0;
+    let binID;
+    for (let v of results) {
+      if (v === null || v === undefined) {
+        ++noResultCount;
+        continue;
+      }
+      if (isNaN(v)) {
+        ++nanCount;
+        continue;
+      }
+      // out-of-range numbers will be placed at the last bin
+      binID = Math.min(Math.floor(v / binSize), numOfBins - 1);
+      ++binCounts[binID];
+    }
+
+    let labels = [];
+    let data = [];
+    let start, end;
+    for (let i = 0; i < numOfBins; ++i) {
+      start = binSize * i;
+      end = start + binSize;
+      if (i < numOfBins - 1) {
+        labels.push(`[${start}, ${end})`);
+      } else {
+        labels.push(`>=${start}`); // notice possible out-of-range values
+      }
+      data.push(binCounts[i])
     }
 
     if (nanCount > 0) {
